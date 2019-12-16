16d4180766189abcd28c43a04f84ee1df1b62d16
Yusaf Osei-Bonsu <yusaf.osei-bonsu@kcl.ac.uk>
2019-12-16 17:28:21 +0000

    Displaying hardcoded data via Quiz.js

--- quiz-app/src/components/answers.js
+++ quiz-app/src/components/answers.js
@@ -1,9 +1,20 @@
 import React from 'react'
 
-const Answers = () => {
-    return (
-        <div className='answer'>
-           <p>Answers</p> 
+const Answers = (props) => {
+    let answers = [props.results.correct_answer];
+    answers = answers.concat(props.results.incorrect_answers);
+
+    /* 
+    var points = [40, 100, 1, 5, 25, 10];
+    points.sort(function(a, b){return 0.5 - Math.random()});
+
+    Try implementing this to randomly display answers
+    */
+
+    console.log(answers);
+    return ( 
+        <div className='answers'>
+            <p></p>
         </div>
     );
 }
