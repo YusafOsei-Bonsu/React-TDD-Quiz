d84d396deca3e3bac1c8b84a07e5a727e7dd622a
Yusaf Osei-Bonsu <yusaf.osei-bonsu@kcl.ac.uk>
2019-12-17 15:11:15 +0000

    Added CSS glowing class to button

--- quiz-app/src/components/start.js
+++ quiz-app/src/components/start.js
@@ -4,7 +4,7 @@ import '../styles/start.css';
 const Start = () => {
     return (
         <div className='start'>
-            <button>Start</button>
+            <button className="glow-on-hover">Start Quiz</button>
         </div>
     )
 }
