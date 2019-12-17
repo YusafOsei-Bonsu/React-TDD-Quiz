230d53aaa721bb865d5cacf1d42ca291abf7ca73
Yusaf Osei-Bonsu <yusaf.osei-bonsu@kcl.ac.uk>
2019-12-17 14:23:13 +0000

    Adding space between code for neatness

--- quiz-app/src/Questions.test.js
+++ quiz-app/src/Questions.test.js
@@ -4,8 +4,11 @@ import Questions from './components/questions'
 
 describe('Questions', () => {
     let wrapper;
+    
     beforeAll(() => wrapper = shallow(<Questions />));
+    
     // checking if file exists
     test('File should exist', () => expect(require('./components/questions')).toBeTruthy());
+    
     afterAll(() => wrapper =null);
 });
