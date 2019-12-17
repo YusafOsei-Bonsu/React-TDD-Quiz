8f6602a9c76fa3e9b9152fd59abf1231ea8a9c47
Yusaf Osei-Bonsu <yusaf.osei-bonsu@kcl.ac.uk>
2019-12-17 17:53:25 +0000

    Update form.js

--- quiz-app/src/components/form.js
+++ quiz-app/src/components/form.js
@@ -1,22 +1,42 @@
 import React from 'react';
 import '../styles/form.css';
-import { connect } from 'react-redux';
+import { connect, Provider } from 'react-redux';
 import { getQuizData } from './api/';
 import { useHistory } from 'react-router-dom';
 
 const Form = (props) => {
     let info = props.categories;
     let history = useHistory();
+
+    let difficultyLevels = {
+        medium: 'medium',
+        easy: 'easy',
+        hard: 'hard' 
+    }
+
     return (
         <div className='form'>
             <form onSubmit={(event) => {props.handleSubmit(event); history.push('/quiz')}}>
                 <select name="topic">
                     {info.map((data) => { return <option key={data.id} value={data.id}>{data.name}</option> })}
                 </select><br />
-                <input type="radio" name="difficulty" value="easy" />
-                <input type="radio" name="difficulty" value="medium" />
-                <input type="radio" name="difficulty" value="hard" /><br/>
-                <input onChange={props.handleChange} type='text' placeholder='player1' /><br/>
+                <label>
+                    <input type="radio" name="difficulty" value={difficultyLevels.easy} />
+                    {difficultyLevels.easy}
+                </label>
+                <br />
+                <label>
+                    <input type="radio" name="difficulty" value={difficultyLevels.medium} />
+                    {difficultyLevels.medium}
+                </label>
+                <br />
+                <label>
+                    <input type="radio" name="difficulty" value={difficultyLevels.hard} />
+                    {difficultyLevels.hard}
+                </label>
+                
+                <br/>
+                <input id='textbox' onChange={props.handleChange} type='text' placeholder='player1' /><br/>
                 <input type="submit" value="Go" />
             </form>
         </div>
