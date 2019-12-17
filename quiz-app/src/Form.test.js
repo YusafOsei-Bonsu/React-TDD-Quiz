00b19f66adb7529eb51c2c863487a11f9864aa95
Yusaf Osei-Bonsu <yusaf.osei-bonsu@kcl.ac.uk>
2019-12-17 12:37:28 +0000

    Test suite for Form comp

--- quiz-app/src/Form.test.js
+++ quiz-app/src/Form.test.js
@@ -4,7 +4,22 @@ import Form from './components/form'
 
 describe('Form component', () => {
     let wrapper;
+
+    // Focuses on the Form component for testing
     beforeAll(() => wrapper = shallow(<Form />));
-    test('Form comp should exist', () => expect(require('./components/form')).toBeTruthy());
+
+    // Checks if the file exists
+    test('File should exist', () => expect(require('./components/form')).toBeTruthy());
+    
+    // Checks if the drop-down box renders
+    test('Dropdown box should render', () => expect(wrapper.find('select').length).toEqual(1));
+
+    // Checks if the difficulty selectors render
+    test('Radiobuttons should render', () => expect(wrapper.find('.levelInputs').length).toEqual(1));
+
+    // Checks if the textbox renders
+    test('Textbox should render', () => expect(wrapper.find('#textbox').length).toEqual(1));
+
+    // Wipes the test suite
     afterAll(() => wrapper =null);
 });
