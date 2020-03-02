fbcc1f5f59a0b660424c42d0715012e437a88399
Yusaf Osei-Bonsu <yusaf.osei-bonsu@kcl.ac.uk>
2020-03-02 18:09:09 +0000

    Sending build dir to Heroku

---
+++ build/precache-manifest.558fd040908ea88af7e6e797937c5d02.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf2b1a2cf7c67f0a25627f6b87c14f82",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a1ba52ab47a269397e07",
+    "url": "/static/css/main.5e239a24.chunk.css"
+  },
+  {
+    "revision": "bbf383d6fa12aec1e814",
+    "url": "/static/js/2.0194b1a0.chunk.js"
+  },
+  {
+    "revision": "7b99baa6d22dfb80026af034d475faa3",
+    "url": "/static/js/2.0194b1a0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a1ba52ab47a269397e07",
+    "url": "/static/js/main.6137d4fb.chunk.js"
+  },
+  {
+    "revision": "fdcdab7d1a93c099f76e",
+    "url": "/static/js/runtime-main.980a9437.js"
+  }
+]);
