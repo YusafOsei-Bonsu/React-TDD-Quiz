81c14d94716d0d3a7942cae4888516ac6b20bf53
Yusaf Osei-Bonsu <yusaf.osei-bonsu@kcl.ac.uk>
2020-03-02 18:03:26 +0000

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ const app = express();
 const port = process.env.PORT || 8080;
 app.use(express.static(path.join(__dirname, "build")));
 
-app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, "build", "index.html")));
+app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, "build", "index.html")));
 
 app.listen(port, () => console.log(`Server listening on port ${port}`));
